bf789fc178ef7bb7870d2441ae1aa23d25f7c86a
EdwinRM <eddwinn.rm@gmail.com>
2023-11-11 10:59:43 -0600

    Initial Commit

---
+++ ios calculator/ios_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_calculatorApp.swift
+//  ios calculator
+//
+//  Created by Edwin Ramirez on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
