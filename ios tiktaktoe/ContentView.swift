af1d09dd6df459f204a3a90921d2df80e2eae291
EdwinRM <eddwinn.rm@gmail.com>
2023-11-16 21:29:11 -0600

    Block double click + create basic ai position

--- ios tiktaktoe/ContentView.swift
+++ ios tiktaktoe/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     let columns: [GridItem]=[GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
     
     @State private var moves:[Move?] = Array(repeating: nil, count: 9)
-    @State private var isHumanTurn = true
+    @State private var isGameboardDisabled = false
     
     var body: some View {
         GeometryReader{geometry in
@@ -30,20 +30,42 @@ struct ContentView: View {
                             Image(systemName:moves[i]?.indicator ?? "").resizable().frame(width: 40,height: 40).foregroundColor(.white)
                         }
                         .onTapGesture {
-                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
-                            isHumanTurn.toggle()
+                            if isSquareOccupied(in: moves, forIndex: i){return}
+                            moves[i] = Move(player:  .human , boardIndex: i)
+                            isGameboardDisabled = true
+                            //check for win condition or draw
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+                                let computerPosition = determineComputerMovePosition(in: moves)
+                                moves[computerPosition] = Move(player:  .computer , boardIndex: computerPosition)
+                                isGameboardDisabled = false
+                            }
+                            
                         }
                     
                     }
                 }
                 Spacer()
             }
+            .disabled(isGameboardDisabled)
             .padding()
            
         }
-       
-        
     }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: { $0?.boardIndex == index})
+    }
+    
+    func determineComputerMovePosition(in moves: [Move?]) -> Int{
+        var movePosition = Int.random(in: 0..<9)
+        
+        while isSquareOccupied(in: moves, forIndex: movePosition){
+            movePosition = Int.random(in: 0..<9)
+        }
+        return movePosition
+    }
+    
 }
 
 enum Player{
