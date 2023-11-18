10758cdbab1f439d79cb3be263e8830a538f1a44
EdwinRM <eddwinn.rm@gmail.com>
2023-11-18 09:50:16 -0600

    If Ai can win, then win

--- ios tiktaktoe/ContentView.swift
+++ ios tiktaktoe/ContentView.swift
@@ -84,9 +84,41 @@ struct ContentView: View {
         return moves.contains(where: { $0?.boardIndex == index})
     }
     
+    // If Ai can win, then win
+    // If AI cant win, then block
+    // If Ai cant block, then take middle square
+    // If Ai cant take middle square, take randowm avaible position
     func determineComputerMovePosition(in moves: [Move?]) -> Int{
-        var movePosition = Int.random(in: 0..<9)
         
+        // If Ai can win, then win
+        let winPatterns: Set<Set<Int>> = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        
+        let computerMoves = moves.compactMap { $0 }.filter{$0.player == .computer}
+        let computerPosition = Set(computerMoves.map { $0.boardIndex})
+        
+        for pattern in winPatterns{
+            let winPosition = pattern.subtracting(computerPosition)
+            
+            if winPosition.count == 1 {
+                let isAvaiable = !isSquareOccupied(in: moves, forIndex: winPosition.first!)
+                if isAvaiable { return winPosition.first!}
+            }
+            
+            
+        }
+        
+        // If AI cant win, then block
+        
+        
+        
+        
+        // If Ai cant block, then take middle square
+        
+        
+    
+        
+        // If Ai cant take middle square, take randowm avaible position
+        var movePosition = Int.random(in: 0..<9)
         while isSquareOccupied(in: moves, forIndex: movePosition){
             movePosition = Int.random(in: 0..<9)
         }
